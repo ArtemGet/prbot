5d2405354c5ebf6ddba5272e731fe1b2bbcb948c
Artem Getmanskii <re910@yandex.ru>
2025-03-02 22:40:53 +0300

    implement Json project

--- src/io/github/artemget/prbot/domain/pr/ProjJson.java
+++ src/io/github/artemget/prbot/domain/pr/ProjJson.java
@@ -1,18 +1,73 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024-2025. Artem Getmanskii
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package io.github.artemget.prbot.domain.pr;
 
-public class ProjJson implements Project {
-    @Override
-    public String name() {
-        return null;
+import javax.json.JsonObject;
+
+/**
+ * Git project from json source.
+ *
+ * @since 0.0.1
+ */
+public final class ProjJson implements Project {
+    /**
+     * Json containing project's name and link.
+     * Suggested structure:
+     * {
+     *   "name": "123",
+     *   "link": "https://example-branch-link.com/123",
+     *   ...
+     * }
+     */
+    private final Branch branch;
+
+    /**
+     * Ctor.
+     *
+     * @param json Project
+     */
+    public ProjJson(final JsonObject json) {
+        this(new BrJson(json));
+    }
+
+    /**
+     * Main Ctor.
+     *
+     * @param branch Reused
+     */
+    private ProjJson(final Branch branch) {
+        this.branch = branch;
     }
 
     @Override
-    public String link() {
-        return null;
+    public String name() throws EmptyArgumentException {
+        return this.branch.name();
     }
 
     @Override
-    public PullRequest request() {
-        return null;
+    public String link() throws EmptyArgumentException {
+        return this.branch.link();
     }
 }
--- src/io/github/artemget/prbot/domain/pr/Project.java
+++ src/io/github/artemget/prbot/domain/pr/Project.java
@@ -1,6 +1,48 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024-2025. Artem Getmanskii
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package io.github.artemget.prbot.domain.pr;
 
+/**
+ * Git project.
+ *
+ * @since 0.0.1
+ */
 public interface Project {
-    String name();
-    String link();
+    /**
+     * Returns git project's name.
+     *
+     * @return Git project's name
+     * @throws EmptyArgumentException If not exists
+     */
+    String name() throws EmptyArgumentException;
+
+    /**
+     * Returns link to project.
+     *
+     * @return Link to project
+     * @throws EmptyArgumentException If not exists
+     */
+    String link() throws EmptyArgumentException;
 }
